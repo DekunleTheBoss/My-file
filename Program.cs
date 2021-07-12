84ec62b65e549df86f2363de30f072694e22270c
DekunleTheBoss <87271523+DekunleTheBoss@users.noreply.github.com>
2021-07-12 23:01:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SHADOW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string course;
+            string inpout;
+
+            //while(true){
+
+                Console.WriteLine("Are you a Per-Time student or Full-Time student: ");
+                inpout = Console.ReadLine();
+
+                if (inpout.Substring(0,4).ToLower().Contains("full"))
+                {
+                    Console.WriteLine("Enter your courde of study: ");
+                    course = Console.ReadLine();
+                    Console.WriteLine(course + "is a good course");
+                    //break;
+                }
+
+                if (inpout.Substring(0,4).ToLower().Contains("part"))
+                {
+                    Console.WriteLine("How many unit are you taking?");
+                    int partTimeCredit = Convert.ToInt32(Console.ReadLine());
+                     if (partTimeCredit > 6)
+                     {
+                         Console.WriteLine("Too much unit for a Part-Time student");
+                         //continue;
+                     }
+                     else
+                     {
+                         Console.WriteLine("proceed");
+                         //break;
+                     }
+                }
+            //}
+
+
+        } 
+    }
+}
